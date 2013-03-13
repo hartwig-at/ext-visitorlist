48b4da18db1cf98b60570166295143572fe6f208
Oliver Salzburg <oliver.salzburg@gmail.com>
2013-03-13 19:04:47 +0100

    Fixed cachable state

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,5 +2,5 @@
 if( !defined( 'TYPO3_MODE' ) ) {
 	die( 'Access denied.' );
 }
-Tx_Extbase_Utility_Extension::configurePlugin(  'user_visitorlist',  'List',  array(    'List' => 'list,excel'  ),  array(    'List' => ''  ));
+Tx_Extbase_Utility_Extension::configurePlugin(  'user_visitorlist',  'List',  array(    'List' => ''  ),  array(    'List' => 'list,excel'  ));
 ?>
